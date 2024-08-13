7754de356a306339a9fb29afb436e4a4a3b79d46
William <bzfishmaster94@gmail.com>
2024-08-12 23:27:38 -0400

    trying to resolve this?

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,10 +54,6 @@ router.get('/game/:id', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 4596b4c7438e41e0be76bb643aad1dba926e2950
 router.get('/genre/:genre', async (req, res) => {
   try {
     const gameGenreSearch = await Games.findAll({
