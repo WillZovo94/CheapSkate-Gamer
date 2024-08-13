4a4d18aad797852e337a54cb5e18dc9c44c04cd6
Charles Thao <thaocharles23@gmail.com>
2024-08-13 13:33:19 -0500

    POST route to make a new review works

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -7,6 +7,7 @@ router.post('/', async (req, res) => {
   try {
     const newReview = await Reviews.create({
       ...req.body,
+      user_id: req.session.user_id
     });
 
     res.status(200).json(newReview);
