951c64d47421c8313c3c0a9bb0a39a3361dfe5e1
TalanvorD <164896317+TalanvorD@users.noreply.github.com>
2024-08-12 19:01:47 -0500

    Collection route and handlebar

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const collectionRoutes = require('./collectionRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/collection', collectionRoutes);
 
 module.exports = router;
